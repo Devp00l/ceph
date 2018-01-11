ccf071fbd5022ad88aa613a7b24b43f127ada0a9
Ricardo Marques <rimarques@suse.com>
2018-01-11 16:51:21 +0000

    mgr/dashboard: add angular support for HTTP calls
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- src/pybind/mgr/dashboard/client/src/app/app.module.ts
+++ src/pybind/mgr/dashboard/client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -15,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     CoreModule,
     CephModule,
