6a5781ece383ba53b9c615436604c239756bf8e7
Stephan Müller <smueller@suse.com>
2018-04-06 10:52:56 +0200

    Example usage of deletion button
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-list/osd-list.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-list/osd-list.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+
 import { OsdService } from '../../../../shared/api/osd.service';
 import { CellTemplate } from '../../../../shared/enum/cell-template.enum';
 import { CdTableColumn } from '../../../../shared/models/cd-table-column';
 import { CdTableSelection } from '../../../../shared/models/cd-table-selection';
 import { DimlessBinaryPipe } from '../../../../shared/pipes/dimless-binary.pipe';
+import {
+  DeletionModalComponent
+} from '../../../../shared/components/deletion-modal/deletion-modal.component';
 
 @Component({
   selector: 'cd-osd-list',
@@ -15,15 +22,62 @@ import { DimlessBinaryPipe } from '../../../../shared/pipes/dimless-binary.pipe'
 export class OsdListComponent implements OnInit {
   @ViewChild('statusColor') statusColor: TemplateRef<any>;
   @ViewChild('osdUsageTpl') osdUsageTpl: TemplateRef<any>;
+  @ViewChild('ctrlDescription') ctrlDescription: TemplateRef<any>;
+  @ViewChild('modalDescription') modalDescription: TemplateRef<any>;
+  someData = [1, 2, 3, 4, 5];
+  finished: number[];
+  ctrlRef: BsModalRef;
+  modalRef: BsModalRef;
+
+  constructor(
+    private osdService: OsdService,
+    private dimlessBinaryPipe: DimlessBinaryPipe,
+    private modalService: BsModalService) {}
+
+  openModal(metaType, pattern, description: TemplateRef<any>) {
+    const ref: BsModalRef = this.modalService.show(DeletionModalComponent);
+    ref.content.metaType = metaType;
+    ref.content.pattern = pattern;
+    ref.content.description = description;
+    ref.content.modalRef = ref;
+    return ref;
+  }
+
+  openCtrlDriven() {
+    this.ctrlRef = this.openModal('Controller delete handling', 'ctrl-test', this.ctrlDescription);
+    this.ctrlRef.content.deletionMethod = this.fakeDeleteController.bind(this);
+  }
+
+  openModalDriven() {
+    this.modalRef = this.openModal('Modal delete handling', 'modal-test', this.modalDescription);
+    this.modalRef.content.deletionObserver = this.fakeDelete();
+  }
+
+  finish() {
+    this.finished = [6, 7, 8, 9];
+  }
+
+  fakeDelete() {
+    return (): Observable<any> => {
+      return new Observable((observer: Subscriber<any>) => {
+        Observable.timer(100).subscribe(() => {
+          observer.next(this.finish());
+          observer.complete();
+        });
+      });
+    };
+  }
+
+  fakeDeleteController() {
+    Observable.timer(100).subscribe(() => {
+      this.finish();
+      this.ctrlRef.hide();
+    });
+  }
 
   osds = [];
   columns: CdTableColumn[];
   selection = new CdTableSelection();
-
-  constructor(
-    private osdService: OsdService,
-    private dimlessBinaryPipe: DimlessBinaryPipe
-  ) { }
 
   ngOnInit() {
     this.columns = [
