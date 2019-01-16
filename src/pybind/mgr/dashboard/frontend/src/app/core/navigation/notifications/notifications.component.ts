485752003ef8ac4e017e55b2a2a248a2d07ae689
Stephan Müller <smueller@suse.com>
2019-01-16 16:55:16 +0100

    mgr/dashboard: Add the Prometheus frontend
    Now Prometheus alerts can be found in "Cluster > Alerts". Incoming
    notifications can be seen as usual in the notifications popover.
    
    To clarify:
    Prometheus alerts are received from the alertmanager API.
    Prometheus alert notification are send from the alertmanager to the
    backend receiver. An alert notification can have multiple alerts, but
    these alerts differ from the prometheus alerts.
    
    To clarify that, I've added some models and services.
    
    If one of the methods to get alerts contains changes the user will be
    notified.
    
    Fixes: https://tracker.ceph.com/issues/36721
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/notifications/notifications.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/notifications/notifications.component.ts
@@ -1,28 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import * as _ from 'lodash';
 
 import { NotificationType } from '../../../shared/enum/notification-type.enum';
 import { CdNotification } from '../../../shared/models/cd-notification';
+import { AuthStorageService } from '../../../shared/services/auth-storage.service';
 import { NotificationService } from '../../../shared/services/notification.service';
+import { PrometheusAlertService } from '../../../shared/services/prometheus-alert.service';
+import { PrometheusNotificationService } from '../../../shared/services/prometheus-notification.service';
 
 @Component({
   selector: 'cd-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss']
 })
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent implements OnInit, OnDestroy {
   notifications: CdNotification[];
-  notificationType = NotificationType;
+  private interval: number;
 
-  constructor(private notificationService: NotificationService) {
+  constructor(
+    private notificationService: NotificationService,
+    private prometheusNotificationService: PrometheusNotificationService,
+    private authStorageService: AuthStorageService,
+    private prometheusAlertService: PrometheusAlertService
+  ) {
     this.notifications = [];
   }
 
+  ngOnDestroy() {
+    window.clearInterval(this.interval);
+  }
+
   ngOnInit() {
+    if (this.authStorageService.getPermissions().prometheus.read) {
+      this.triggerPrometheusAlerts();
+      this.interval = window.setInterval(() => {
+        this.triggerPrometheusAlerts();
+      }, 5000);
+    }
     this.notificationService.data$.subscribe((notifications: CdNotification[]) => {
       this.notifications = _.orderBy(notifications, ['timestamp'], ['desc']);
     });
+  }
+
+  private triggerPrometheusAlerts() {
+    this.prometheusAlertService.refresh();
+    this.prometheusNotificationService.refresh();
   }
 
   removeAll() {
