d4d2ae1ae6eb3bcc97e274d757d461e6f73211bb
Stephan Müller <smueller@suse.com>
2019-07-10 16:27:53 +0200

    Some script changes
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/update-codecov.sh
+++ src/pybind/mgr/dashboard/update-codecov.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-bash <(curl -s https://codecov.io/bash) -c -F dashboard -y src/pybind/mgr/dashboard/codecov.yml -s src/pybind/mgr/dashboard -t 70e39e58-fa7d-40bf-a428-0925eb779c33
+# $1 = Pull request number in order to attach report
+
+function codecov {
+  bash <(curl -s https://codecov.io/bash) -y src/pybind/mgr/dashboard/codecov.yml -s . -t 70e39e58-fa7d-40bf-a428-0925eb779c33
+}
+
+if [ -n "$1" ]; then
+  codecov -P $1
+else
+  codecov
+fi
