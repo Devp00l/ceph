0ff71de888da590bde501521c06047f1e3696568
Stephan Müller <smueller@suse.com>
2019-07-11 17:29:21 +0200

    try without the flag on - may be now the config will do something
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/update-codecov.sh
+++ src/pybind/mgr/dashboard/update-codecov.sh
@@ -2,7 +2,7 @@
 # $1 = Pull request number in order to attach report
 
 function codecov {
-  bash <(curl -s https://codecov.io/bash) -X fix -s "$(dirname $0)" -F dashboard -t 70e39e58-fa7d-40bf-a428-0925eb779c33
+  bash <(curl -s https://codecov.io/bash) -X fix -s "$(dirname $0)" -t 70e39e58-fa7d-40bf-a428-0925eb779c33
 }
 
 if [ -n "$1" ]; then
