7a8a671c58af380468d4fdaf10ec97f4a9a40c06
Stephan Müller <smueller@suse.com>
2019-07-15 14:33:28 +0200

    Fixed exit status and absolute path
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/run-by-travis.sh
+++ src/pybind/mgr/dashboard/run-by-travis.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-DASHBOARD=$(dirname $0)
+failed=false
+DASHBOARD=$PWD/src/pybind/mgr/dashboard
 
 # Run backend unit tests
 cd $DASHBOARD
-pip3 install --user -r requirements.txt
-tox `readlink -f tox.ini` -e "py3-cov"
+pip3.7 install --user -r requirements.txt
+tox `readlink -f tox.ini` -e "py3-cov" || failed=true
 
 
 # Run frontend unit tests
 cd $DASHBOARD/frontend
 npm install
-npm run test:ci
+npm run test:ci || failed=true
 
 # Export coverage
 bash <(curl -s https://codecov.io/bash) -X fix -s "$DASHBOARD" -F dashboard
+
+if [ "$failed" = "true" ]; then
+  exit 1
+fi
