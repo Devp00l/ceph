3b0dfdbae8bc03bb6044b9cb51b669ff81329932
Stephan Müller <smueller@suse.com>
2019-07-15 14:50:40 +0200

    Fixes fronend unit tests
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/run-by-travis.sh
+++ src/pybind/mgr/dashboard/run-by-travis.sh
@@ -3,18 +3,20 @@
 failed=false
 DASHBOARD=$PWD/src/pybind/mgr/dashboard
 
-# Run backend unit tests
+echo "Run backend unit tests"
 cd $DASHBOARD
 pip3.7 install --user -r requirements.txt
 tox `readlink -f tox.ini` -e "py3-cov" || failed=true
 
 
-# Run frontend unit tests
+echo "Run frontend unit tests"
 cd $DASHBOARD/frontend
 npm install
+cp src/environments/environment.tpl.ts src/environments/environment.prod.ts
+cp src/environments/environment.tpl.ts src/environments/environment.ts
 npm run test:ci || failed=true
 
-# Export coverage
+echo "Send coverage reports to codecov"
 bash <(curl -s https://codecov.io/bash) -X fix -s "$DASHBOARD" -F dashboard
 
 if [ "$failed" = "true" ]; then
