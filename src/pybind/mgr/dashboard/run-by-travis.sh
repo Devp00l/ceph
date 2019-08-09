abb8d6acc8479988efcf2b7882c0fbfe356eab6c
Stephan Müller <smueller@suse.com>
2019-08-09 12:03:12 +0200

    Faster runs
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/run-by-travis.sh
+++ src/pybind/mgr/dashboard/run-by-travis.sh
@@ -2,13 +2,20 @@
 
 failed=false
 DASHBOARD=$PWD/src/pybind/mgr/dashboard
+setupVirtualenv=$PWD/src/tools/setup-virtualenv.sh
+
+echo "Setup virtualenv"
+cd $DASHBOARD
+$setupVirtualenv /ceph/build/mgr-dashboard-virtualenv
 
 echo "Run backend unit tests"
-cd $DASHBOARD
-: ${CEPH_BUILD_DIR:=$DASHBOARD/../.tox}
-mkdir $CEPH_BUILD_DIR
-tox -e "py3" || failed=true
+#tox -e py3,check || failed=true
+WITH_PYTHON2=OFF ../../../script/run_tox.sh --tox-env py3 || failed=true
 
+if [ "$failed" = "true" ]; then
+  echo "Backend unit tests have failed!"
+  exit 1
+fi
 
 echo "Run frontend unit tests"
 cd $DASHBOARD/frontend
@@ -17,9 +24,11 @@ cp src/environments/environment.tpl.ts src/environments/environment.prod.ts
 cp src/environments/environment.tpl.ts src/environments/environment.ts
 npm run test:ci || failed=true
 
-echo "Send coverage reports to codecov"
-bash <(curl -s https://codecov.io/bash) -X fix -s "$DASHBOARD" -F dashboard
-
 if [ "$failed" = "true" ]; then
+  echo "Frontend unit tests have failed!"
   exit 1
 fi
+
+echo "Send coverage reports to codecov"
+bash <(curl -s https://codecov.io/bash) -X fix -s "$DASHBOARD" -F dashboard
+echo "Everything clear - good job :)"
