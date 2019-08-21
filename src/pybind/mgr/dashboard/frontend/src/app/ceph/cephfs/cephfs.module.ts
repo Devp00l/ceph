7d6914cdcedd68d6ae86af71e82de481e9411317
Stephan Müller <smueller@suse.com>
2019-08-21 10:34:02 +0200

    mgr/dashboard: CephFS directory component
    Now it's possible to list directories of a CephFS in a tree view inside
    the dashboard.
    
    Currently the tree is not refreshed every 5 seconds as the listing of
    directories is a heavier operation.
    
    When a directory is clicked, the dashboard will fetch it's
    subdirectories and the subdirectories of it's subdirectories in order to
    show if the subdirectories have subdirectories.
    
    The details of a directory show the full path, it's quota values and
    the path they originate from and a list of all snapshots.
    
    Fixes: https://tracker.ceph.com/issues/41575
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ChartsModule } from 'ng2-charts';
+import { TreeModule } from 'ng2-tree';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 
@@ -10,6 +12,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { CephfsChartComponent } from './cephfs-chart/cephfs-chart.component';
 import { CephfsClientsComponent } from './cephfs-clients/cephfs-clients.component';
 import { CephfsDetailComponent } from './cephfs-detail/cephfs-detail.component';
+import { CephfsDirectoriesComponent } from './cephfs-directories/cephfs-directories.component';
 import { CephfsListComponent } from './cephfs-list/cephfs-list.component';
 import { CephfsTabsComponent } from './cephfs-tabs/cephfs-tabs.component';
 
@@ -19,6 +22,8 @@ import { CephfsTabsComponent } from './cephfs-tabs/cephfs-tabs.component';
     SharedModule,
     AppRoutingModule,
     ChartsModule,
+    TreeModule,
+    FormsModule,
     ProgressbarModule.forRoot(),
     TabsModule.forRoot()
   ],
@@ -27,7 +32,8 @@ import { CephfsTabsComponent } from './cephfs-tabs/cephfs-tabs.component';
     CephfsClientsComponent,
     CephfsChartComponent,
     CephfsListComponent,
-    CephfsTabsComponent
+    CephfsTabsComponent,
+    CephfsDirectoriesComponent
   ]
 })
 export class CephfsModule {}
