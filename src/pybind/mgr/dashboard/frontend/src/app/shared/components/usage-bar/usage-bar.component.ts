65bd2d6a387dc5d2be22f254232f543a9417e40e
Avan Thakkar <athakkar@redhat.com>
2020-04-17 14:21:48 +0530

    mgr/dashboard: Display users current bucket quota usage Fixes: https://tracker.ceph.com/issues/45011
    Signed-off-by: Avan Thakkar <athakkar@redhat.com>
    (cherry picked from commit 966d887f7a5e0765b4210ab4edcb1cea3e03ac35)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts
@@ -7,17 +7,26 @@ import { Component, Input, OnChanges } from '@angular/core';
 })
 export class UsageBarComponent implements OnChanges {
   @Input()
+  totalAmount: number;
+  @Input()
+  usedAmount: number;
+  @Input()
   totalBytes: number;
   @Input()
   usedBytes: number;
 
+  freeAmount: number;
+  freeBytes: number;
   usedPercentage: number;
   freePercentage: number;
-  freeBytes: number;
 
   ngOnChanges() {
-    this.usedPercentage = Math.round((this.usedBytes / this.totalBytes) * 100);
-    this.freePercentage = 100 - this.usedPercentage;
+    this.freeAmount = this.totalAmount - this.usedAmount;
     this.freeBytes = this.totalBytes - this.usedBytes;
+    this.usedPercentage =
+      this.totalAmount > 0
+        ? Math.round((this.usedAmount / this.totalAmount) * 100)
+        : Math.round((this.usedBytes / this.totalBytes) * 100);
+    this.freePercentage = 100 - this.usedPercentage;
   }
 }
